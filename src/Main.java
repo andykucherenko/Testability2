31b7090421d3ac6a6c8ec2e558935f74cce62a55
Andrey Kucherenko <andykucherenko@gmail.com>
2023-02-13 10:45:34 +0300

    Calculation

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.87;
-        double weight = 98;
+        double height = 1.87; // метры
+        double weight = 98;   // килограммы
         int index = (int) service.calculate(height, weight);
 
         System.out.println(index);
